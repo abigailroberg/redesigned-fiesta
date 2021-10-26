5f3513aebc081fee081d4425ae910a0d05642b40
Abigail Roberg <abbyroberg@gmail.com>
2021-10-26 10:27:42 -0500

    user post, put, and delete controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -18,10 +18,37 @@ const userController = {
             .select('-__v')
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.status(400).json(err))
-    }
-    // post new user
+    },
+    // create new user (expects { "username": "username", "email": "email@mail.com" })
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err))
+    },
     // update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'No user with that id' })
+                    return
+                }
+                res.json(dbUserData)
+            })
+            .catch(err => res.status(400).json(err))
+    },
     // delete user by id
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'No user with that id' })
+                    return
+                }
+                res.json({ message: 'Successfully deleted!' })
+            })
+            .catch(err => res.status(400).json(err))
+    }
 }
 
 module.exports = userController
