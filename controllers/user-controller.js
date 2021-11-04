9378a424b0d42afc95204a728ffbc3f4fdb1d3e9
Abigail Roberg <abbyroberg@gmail.com>
2021-11-04 13:03:58 -0500

    set up thought routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,4 @@
 const User = require('../models/User.js')
-const Thought = require('../models/Thought')
 
 const userController = {
     // get all users
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+
+const {
+    getAllThoughts
+} = require('../../controllers/thought-controller')
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+
+module.exports = router
